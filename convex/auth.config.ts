3bb999c4fe0e8756f45dd86319edeb6158e252b0
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-05-09 04:13:41 +0530

    feat: voice assistant setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// https://emerging-starfish-56.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: "https://emerging-starfish-56.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ],
+  };
